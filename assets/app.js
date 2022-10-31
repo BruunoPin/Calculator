b4313990d4802efc23daad224584c74b2d65f1bf
Bruno Pinheiro <brunopinheiro.oli@gmail.com>
2022-10-31 13:19:05 -0300

    melhorando js

--- assets/app.js
+++ assets/app.js
@@ -1,43 +1,24 @@
-const calculator = document.querySelector(".calculator");
 const placeNumber = document.getElementById("place-number");
-const button0 = document.getElementById("0");
-const button1 = document.getElementById("1");
-const button2 = document.getElementById("2");
-const button3 = document.getElementById("3");
-const button4 = document.getElementById("4");
-const button5 = document.getElementById("5");
-const button6 = document.getElementById("6");
-const button7 = document.getElementById("7");
-const button8 = document.getElementById("8");
-const button9 = document.getElementById("9");
-const sum = document.getElementById("+");
-const sub = document.getElementById("-");
-const divide = document.getElementById("/");
-const mult = document.getElementById("*");
-const percent = document.getElementById("%");
-const equal = document.getElementById("=");
-const clear = document.getElementById("ac");
+let buttons = document.querySelectorAll("button");
 
-calculator.addEventListener("click", (event) => {
-  const valor = event.target.value;
-  let teste = [];
-  placeNumber.innerText += valor;
+buttons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    const valor = event.target.value;
 
-  if (valor === "ac") {
-    placeNumber.innerText = "";
-  }
-  teste.push(placeNumber.innerText);
-  console.log(teste);
+    valor === "%" ?
+      (placeNumber.value = eval(placeNumber.value / 100)) :
+      (placeNumber.value += valor);
 
-  const calculadora = () => {
-    if (event.target.textContent === "=") {
-      console.log(teste);
+    if (valor === "ac") {
+      placeNumber.value = "";
     }
-  };
 
-  calculadora();
+    const calculadora = () => {
+      if (event.target.textContent === "=") {
+        placeNumber.value = eval(placeNumber.value);
+      }
+    };
 
-  // console.log(placeNumber.textContent);
-
-  // console.log(placeNumber.textContent=event.target.value);
+    calculadora();
+  });
 });
